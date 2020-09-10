aaa34010b8975d7d4bfb440906e11e8c80f21170
Baur Juergen <juergen.baur@avisco.de>
2020-09-10 08:15:16 +0000

    Week4 Unit2 UPDATE AND MANIPULATE DATA

--- webapp/controller/Add.controller.js
+++ webapp/controller/Add.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"opensap/products/ManageProducts/controller/BaseController",
-	"sap/ui/core/routing/History"
-], function (BaseController, History) {
+	"sap/ui/core/routing/History",
+	"sap/m/MessageToast"
+], function (BaseController, History, MessageToast) {
 	"use strict";
 
 	return BaseController.extend("opensap.products.ManageProducts.controller.Add", {
@@ -23,12 +24,66 @@ sap.ui.define([
 		/* =========================================================== */
 		/* event handlers                                              */
 		/* =========================================================== */
+		/**
+		 * Event handler for the cancel action
+		 * @public
+		 */
+		onCancel: function () {
+			this.onNavBack();
+		},
+
+		/**
+		 * Event handler for the save action
+		 * @public
+		 */
+		onSave: function () {
+			this.getModel().submitChanges();
+		},
 
 		_onRouteMatched: function () {
+			// register for metadata loaded events
+			var oModel = this.getModel();
+			oModel.metadataLoaded().then(this._onMetadataLoaded.bind(this));
+		},
 
-			//here goes your logic which will be executed when the "add" route is hit
-			//will be done within the next unit
+		_onMetadataLoaded: function () {
 
+			// create default properties
+			var oProperties = {
+				ProductID: "" + parseInt(Math.random() * 1000000000, 10),
+				TypeCode: "PR",
+				TaxTarifCode: 1,
+				CurrencyCode: "EUR",
+				MeasureUnit: "EA"
+			};
+
+			// create new entry in the model
+			this._oContext = this.getModel().createEntry("/ProductSet", {
+				properties: oProperties,
+				success: this._onCreateSuccess.bind(this)
+			});
+
+			// bind the view to the new entry
+			this.getView().setBindingContext(this._oContext);
+
+		},
+
+		_onCreateSuccess: function (oProduct) {
+
+			// navigate to the new product's object view
+			var sId = oProduct.ProductID;
+			this.getRouter().navTo("object", {
+				objectId: sId
+			}, true);
+
+			// unbind the view to not show this object again
+			this.getView().unbindObject();
+
+			// show success messge
+			var sMessage = this.getResourceBundle().getText("newObjectCreated", [oProduct.Name]);
+			MessageToast.show(sMessage, {
+				closeOnBrowserNavigation: false
+			});
 		},
 
 		/**
@@ -38,6 +93,8 @@ sap.ui.define([
 		 * @public
 		 */
 		onNavBack: function () {
+			// discard new product from model.
+			this.getModel().deleteCreatedEntry(this._oContext);
 
 			var oHistory = History.getInstance(),
 				sPreviousHash = oHistory.getPreviousHash();
